f0f1c01ff8fb8fa94d3ff32cde1f371b782f3ad9
LeeSeongMin123 <42796952+LeeSeongMin123@users.noreply.github.com>
2020-09-05 01:15:57 +0900

    Update 2750.java

--- codeBAEKJOON/2750/2750.java
+++ codeBAEKJOON/2750/2750.java
@@ -1,2 +1,18 @@
-afsdf
-FD
+import java.util.*;
+
+public class Main {
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int [] Arr = new int[N];
+		for(int i=0; i<N; i++)
+			Arr[i] = sc.nextInt();
+		
+		Arrays.sort(Arr);
+		
+		for(int i=0; i<N; i++) {
+			System.out.print(Arr[i] + "\n");
+		}
+	}
+}
