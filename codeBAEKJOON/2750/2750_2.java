4d4dc79789c492646766dce70156f8fc4a3804b7
LeeSeongMin123 <42796952+LeeSeongMin123@users.noreply.github.com>
2020-09-05 02:25:28 +0900

    Using [ BufferedReader + Array + Arrays.sort ]

--- codeBAEKJOON/2750/2750_2.java
+++ codeBAEKJOON/2750/2750_2.java
@@ -1,1 +1,19 @@
+import java.util.*;
+import java.io.*;
 
+public class Main {
+	public static void main (String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int [] Arr = new int[N];
+		for(int i=0; i<N; i++) 
+			Arr[i] = Integer.parseInt(br.readLine());
+		
+		Arrays.sort(Arr);
+		
+		for(int i=0; i<N; i++)
+			System.out.println(Arr[i]);
+	}
+}
